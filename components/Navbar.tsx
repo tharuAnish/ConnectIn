e75a6dd9e3a5381d47b9ef7e672aa6163385403c
tharuAnish <namoanishtharu@gmail.com>
2025-01-05 17:10:27 +0530

    Initial project file with nav bar

---
+++ components/Navbar.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+import React from "react"
+import SearchInput from "./SearchInput"
+import NavItems from "./NavItems"
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full bg-white z-50">
+      <div className="flex items-center max-w-6xl justify-between h-14 mx-auto px-3">
+        <div className="flex items-center gap-2">
+          <Image src={"/Linkedin_icon.png"} alt="Logo" width={35} height={35} />
+          <div>
+            <SearchInput />
+          </div>
+        </div>
+
+        <div className="flex items-center gap-5">
+          <div>
+            <NavItems />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ components/SearchInput.tsx
@@ -0,0 +1,15 @@
+import { Input } from "./ui/input"
+
+const SearchInput = () => {
+  return (
+    <div>
+      <Input
+        type="textx"
+        placeholder="search"
+        className="bg-[#EDF3F8] w-80 rounded-lg border-none"
+      />
+    </div>
+  )
+}
+
+export default SearchInput
