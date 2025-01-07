bfbf5cf71c4bd385bd3f20ae6f4aa9ff63a6d315
tharuAnish <namoanishtharu@gmail.com>
2025-01-07 17:35:06 +0530

    Caching, revalidate path implemented

--- lib/serveractions.ts
+++ lib/serveractions.ts
@@ -5,6 +5,7 @@ import { IUser } from "@/models/user.model"
 import { currentUser } from "@clerk/nextjs/server"
 import { v2 as cloudinary } from "cloudinary"
 import connectDB from "./db"
+import { revalidatePath } from "next/cache"
 
 // Configure Cloudinary
 cloudinary.config({
@@ -51,6 +52,7 @@ export const createPostAction = async (
         user: userDatabase,
       })
     }
+    revalidatePath("/")
   } catch (error: any) {
     throw new Error(error)
   }
