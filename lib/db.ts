7091734210016154a31a8b8a8a753234169608ce
tharuAnish <namoanishtharu@gmail.com>
2025-01-07 08:47:20 +0530

    user post comment schema build

---
+++ lib/db.ts
@@ -0,0 +1,20 @@
+import mongoose, { Connection } from "mongoose"
+
+let isConnected: Connection | boolean = false
+
+const connectDB = async () => {
+  if (isConnected) {
+    console.log("MongoDB alredy connectced")
+    return isConnected
+  }
+  try {
+    const res = await mongoose.connect(process.env.MONGO_URI!)
+    isConnected = res.connection
+    console.log("MongoDB Connected")
+    return isConnected
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default connectDB
---
+++ models/post.model.ts
@@ -0,0 +1,56 @@
+import mongoose, { Document, Model } from "mongoose"
+import { IUser } from "./user.model"
+import { IComment } from "./comment.model"
+
+export interface IPost {
+  description: string
+  user: IUser
+  imageUrl?: String
+  likes?: string[]
+  comments?: IComment[]
+}
+
+export interface IPostDocument extends IPost, Document {
+  createdAt: Date
+  updatedAt: Date
+}
+
+const postSchema = new mongoose.Schema<IPostDocument>(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    user: {
+      userId: {
+        type: String,
+        required: true,
+      },
+      profilePhoto: {
+        type: String,
+        required: true,
+      },
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+    },
+    likes: {
+      type: [String],
+    },
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  { timestamps: true }
+)
+
+export const Post: Model<IPostDocument> =
+  mongoose.models?.Post || mongoose.model<IPostDocument>("Post", postSchema)
